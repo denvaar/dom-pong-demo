4c7b90402681aa91499c172605fc1b40b5bf4eae
Denver Smith <denverpsmith@gmail.com>
2017-08-29 22:17:04 -0600

    Create initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var isProductionBuild = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: (function () {
+    var plugins = [];
+    if (isProductionBuild) {
+      plugins.push(new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        }
+      }));
+      plugins.push(new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compress: {
+          warnings: false
+        }
+      }));
+    } else {
+      /* development-specific plugins here */
+    }
+    return plugins;
+  })(),
+  module: {
+    loaders: [
+      {
+        test: /\.js?|\.jsx$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        exclude: /node_modules/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.ico$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=[name].[ext]' // favicon
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  }
+};
+
